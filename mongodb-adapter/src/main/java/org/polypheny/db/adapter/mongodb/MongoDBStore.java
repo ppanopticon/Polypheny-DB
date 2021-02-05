2c8f7af18781a29ff317e56de176d839d549d4c3
datomo <dave.lengw@gmail.com>
2021-02-05 16:52:28 +0100

    prototype deploying and removing of mongodb through docker working

--- mongodb-adapter/src/main/java/org/polypheny/db/adapter/mongodb/MongoDBStore.java
+++ mongodb-adapter/src/main/java/org/polypheny/db/adapter/mongodb/MongoDBStore.java
@@ -1,6 +1,15 @@
 package org.polypheny.db.adapter.mongodb;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import com.google.common.collect.ImmutableList;
+import lombok.extern.slf4j.Slf4j;
 import org.polypheny.db.adapter.DataStore;
 import org.polypheny.db.catalog.entity.CatalogColumn;
 import org.polypheny.db.catalog.entity.CatalogColumnPlacement;
@@ -15,6 +24,7 @@ import org.polypheny.db.transaction.PolyXid;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class MongoDBStore extends DataStore {
 
     @SuppressWarnings("WeakerAccess")
@@ -23,13 +33,41 @@ public class MongoDBStore extends DataStore {
     public static final String DESCRIPTION = "MongoDB is a document-based and distributed database. ";
     @SuppressWarnings("WeakerAccess")
     public static final List<AdapterSetting> AVAILABLE_SETTINGS = ImmutableList.of(
-            new AdapterSettingList( "type", false, true, false, ImmutableList.of( "Mongo", "Fongo(embedded)" ) ),
-            new AdapterSettingString( "host", false, true, false, "localhost" ),
-            new AdapterSettingInteger( "port", false, true, false, 27017 )
+            new AdapterSettingList("type", false, true, false, ImmutableList.of("Mongo", "Fongo(embedded)")),
+            new AdapterSettingString("host", false, true, false, "localhost"),
+            new AdapterSettingInteger("port", false, true, false, 27017)
     );
 
-    public MongoDBStore(int adapterId, String uniqueName, Map<String, String> settings, boolean persistent) {
-        super(adapterId, uniqueName, settings, persistent);
+    private DockerClient client;
+
+    public MongoDBStore(int adapterId, String uniqueName, Map<String, String> settings) {
+        super(adapterId, uniqueName, settings, false);
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost("tcp://localhost:2375")
+                .build();
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .build();
+
+        client = DockerClientImpl.getInstance(config, httpClient);
+        client.pingCmd().exec();
+
+        PullImageResultCallback callback = new PullImageResultCallback();
+
+        client.pullImageCmd("mongo:latest").exec(callback);
+        try {
+            // TODO: blocking for now, maybe change or show warning?
+            callback.awaitCompletion();
+            log.warn("finished");
+            client.createContainerCmd("mongo")
+                    .withName(uniqueName)
+                    .withExposedPorts(ExposedPort.parse(settings.get("port")))
+                    .exec();
+            client.startContainerCmd(uniqueName).exec();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -74,12 +112,13 @@ public class MongoDBStore extends DataStore {
 
     @Override
     public List<AdapterSetting> getAvailableSettings() {
-        return null;
+        return AVAILABLE_SETTINGS;
     }
 
     @Override
     public void shutdown() {
-
+        client.stopContainerCmd(getUniqueName()).exec();
+        client.removeContainerCmd(getUniqueName()).exec();
     }
 
     @Override
